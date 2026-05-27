6bf956c6e015b6d531a85d31443094b63f5311e2
asim4998 <asimkumar4998@gmail.com>
2026-05-28 01:17:15 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,23 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Redirect home root to the proxy path
 app.get('/', (req, res) => {
     res.redirect('/main');
 });
 
+// Proxy setup targeting asim.com
 app.use('/main', createProxyMiddleware({
-    target: 'https://pronhub.com',
+    target: 'https://xnxx.com',
     changeOrigin: true,
-    pathRewrite: {
-        '^/main': '',
-    },
-    ws: true
+    logger: console,
+    on: {
+        proxyReq: (proxyReq, req, res) => {
+            proxyReq.setHeader('host', 'xnxx.com');
+        }
+    }
 }));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
-
